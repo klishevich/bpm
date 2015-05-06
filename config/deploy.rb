42b44947e16d7b40f8e6d7b597f6bcb4d2b21df1
klishevich <busation@gmail.com>
2015-05-06 23:17:41 +0300

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :scm, "git"
 set :repository, "git@github.com:klishevich/#{application}.git"
 set :branch, "master"
 
-set :rvm_ruby_string, '2.1.4@busation'
+# set :rvm_ruby_string, '2.1.4@busation'
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
