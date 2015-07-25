ebabdfcb8e5f6e158301d3a215e46aca7fed35e3
klishevich <busation@gmail.com>
2015-07-25 16:36:41 +0300

    improve tests

--- spec/features/new_requests_spec.rb
+++ spec/features/new_requests_spec.rb
@@ -1,17 +1,29 @@
 require 'rails_helper'
 require 'pp'
 
-RSpec.describe "NewRequests", :type => :request do
-  it "login page is OK" do
+RSpec.describe "User", :type => :request do
+  it "can login page and create request" do
     # user = FactoryGirl.create(:user, :username => "mike@gmail.com", :password => "password")
     visit "/"
     # pp "test"
-	expect(page).to have_selector('a', text: 'BPM on Rails') 
+    expect(page).to have_selector('a', text: 'BPM on Rails') 
 
     fill_in "Email", :with => "manager1@test.co"
     fill_in "Password", :with => "mihael1st"
     click_button "enter"
 
     expect(page).to have_selector('a.user_email', :text => 'manager1@test.co')
+
+    visit "/req_reassigns/new"
+    expect(page).to have_selector('h1#new')
+
+    fill_in "req_reassign_name", :with => "test1"
+    fill_in "req_reassign_inn", :with => "1234567890"
+    select 'manager2@test.co', :from => 'req_reassign_manager'
+    # fill_in "req_reassign_manager", :with => "manager2@test.co"
+    fill_in "req_reassign_money", :with => "1111"
+    click_button "save"
+    expect(page).to have_selector('h1#show')
   end
+
 end
