b5823331c8c330413a2948033bc5dc7cde0bff48
klishevich <busation@gmail.com>
2015-09-06 23:45:11 +0300

    improve show req_purchases

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def is_active_controller?(controller_name)
+        params[:controller] == controller_name ? "active" : nil
+    end
+
+    def is_active_action?(action_name)
+        params[:action] == action_name ? "active" : nil
+    end	
 end
