591f183e5e815ce5880f9cd0dfc653b0acfe4726
klishevich <busation@gmail.com>
2015-10-03 13:28:10 +0300

    mailer production setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,14 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'bpm.j123.ru' }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.sendmail_settings = { :arguments => "-i" } 
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
