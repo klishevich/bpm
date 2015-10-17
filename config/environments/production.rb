9da48a85b91cf0d6abc67dedf2621c0b0a8d47f5
klishevich <busation@gmail.com>
2015-10-18 00:29:20 +0300

    prod ru locale

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.i18n.default_locale = :ru
+  config.i18n.available_locales = :ru
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
