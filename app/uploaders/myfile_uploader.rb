cfb8fc8e7274ce933a4732cb51fa651ba7408d9f
klishevich <busation@gmail.com>
2015-10-25 23:37:57 +0300

    finish files

--- app/uploaders/myfile_uploader.rb
+++ app/uploaders/myfile_uploader.rb
@@ -18,7 +18,11 @@ class MyfileUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "system/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def cache_dir
+    'system/uploads/tmp'
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
